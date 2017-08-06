ff11b00f9e7feb7c6296e7c47a1c95fa633d956b
charles-ordina <charles.brands@ordina.nl>
2017-08-06 11:26:37 +0200

    fixed long line errors

--- README.md
+++ README.md
@@ -2,7 +2,19 @@
 This project is a website that shows movie trailers.
 It is the first project for the Udacity Full stack developer nanodegree
 
-To run this program go to the movies folder and type
-python entertainment-center.py
+This program is developed and tested with python version 2.7.10. This code is checked on http://pep8online.com/checkresult.
 
-This program is developed and tested with python version 2.7.10
+## Requirements
+* Python 2.7.x must be installed.
+* The user must know how to start a terminal window and execute commends.
+
+## To run this program
+* Unpack the zipfile to a directory of your choice.
+* Start the terminal.
+* Type the following commands
+	* cd (The directory you unpacked the zipfile to.)
+	* cd ufsd-p1-movie-trailer-website
+	* cd movies
+	* python entertainment-center.py
+* The program wil now generate the movies website and open a browswer window showing the posters of 6 movies. 
+* Clicking on a poster will start a trailer of the movie.
--- movies/entertainment-center.py
+++ movies/entertainment-center.py
@@ -3,54 +3,42 @@ import media
 
 
 # Define the six movies
-movie_1_poster = "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg"
-movie_1_trailer = "https://www.youtube.com/watch?v=vwyZH85NQC4"
 toy_story = media.Movie(
     "Toy Story",
     "A story of a boy and his toys that come to life",
-    movie_1_poster,
-    movie_1_trailer)
+    "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+    "https://www.youtube.com/watch?v=vwyZH85NQC4")
 
-movie_2_poster = "https://i0.wp.com/media2.slashfilm.com/slashfilm/wp/wp-content/images/dunkirk-poster.jpg"
-movie_2_trailer = "https://www.youtube.com/watch?v=F-eMt3SrfFU"
 dunkirk = media.Movie(
     "Dunkirk",
     "A movie about the battle of Dunkirk",
-    movie_2_poster,
-    movie_2_trailer)
+    "https://i0.wp.com/media2.slashfilm.com/slashfilm/wp/wp-content/images/dunkirk-poster.jpg",  # noqa
+    "https://www.youtube.com/watch?v=F-eMt3SrfFU")
 
-movie_3_poster = "https://image.tmdb.org/t/p/original/7j3xCcZZUDTPuOt6KIVDy7xsMyq.jpg"
-movie_3_trailer = "https://www.youtube.com/watch?v=IeOxo7o9T8Q"
 jobs = media.Movie(
     "Steve Jobs",
     "A movie about Steve Jobs",
-    movie_3_poster,
-    movie_3_trailer)
+    "https://image.tmdb.org/t/p/original/7j3xCcZZUDTPuOt6KIVDy7xsMyq.jpg",
+    "https://www.youtube.com/watch?v=IeOxo7o9T8Q")
 
-movie_4_poster = "http://www.superheromovies.net/img/p/6v0OGYaB8510nH0zcKH0ZWP8HWD.jpg"
-movie_4_trailer = "https://www.youtube.com/watch?v=xEvV3OsE2WM"
 spiderman = media.Movie(
     "Spiderman",
     "A movie about spiderman",
-    movie_4_poster,
-    movie_4_trailer)
+    "http://www.superheromovies.net/img/p/6v0OGYaB8510nH0zcKH0ZWP8HWD.jpg",
+    "https://www.youtube.com/watch?v=xEvV3OsE2WM")
 
-movie_5_poster = "http://orig04.deviantart.net/7518/f/2016/207/c/2/justice_league_movie____2017__poster_by_mrdeks-dabh40c.jpg"
-movie_5_trailer = "https://www.youtube.com/watch?v=3cxixDgHUYw"
 justice_league = media.Movie(
     "Justice League",
     "No idea but the trailer was cool",
-    movie_5_poster,
-    movie_5_trailer)
+    "http://orig04.deviantart.net/7518/f/2016/207/c/2/justice_league_movie____2017__poster_by_mrdeks-dabh40c.jpg",  # noqa
+    "https://www.youtube.com/watch?v=3cxixDgHUYw")
 
-movie_6_poster = "https://s-media-cache-ak0.pinimg.com/originals/a8/e9/0a/a8e90a844bed9e6951de16f5f6cde1e0.jpg"
-movie_6_trailer = "https://www.youtube.com/watch?v=gCcx85zbxz4"
 blade_runner = media.Movie(
     "Blade runner",
     "A remake of blade runner",
-    movie_6_poster,
-    movie_6_trailer)
+    "https://s-media-cache-ak0.pinimg.com/originals/a8/e9/0a/a8e90a844bed9e6951de16f5f6cde1e0.jpg",  # noqa
+    "https://www.youtube.com/watch?v=gCcx85zbxz4")
 
-# Put the movies in an array and generate the website with fresh_tomatoes
+# Put the movies in an list and generate the website with fresh_tomatoes
 movies = [toy_story, dunkirk, jobs, spiderman, justice_league, blade_runner]
 fresh_tomatoes.open_movies_page(movies)
