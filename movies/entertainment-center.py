7eac98a26660f12d430eac24b54427ff6bf2241a
charles-ordina <charles.brands@ordina.nl>
2017-08-06 10:34:01 +0200

    added comments

--- movies/entertainment-center.py
+++ movies/entertainment-center.py
@@ -3,41 +3,53 @@ import media
 
 
 # Define the six movies
-movie_1_title = "Toy Story"
-movie_1_story = "A story of a boy and his toys that come to life"
 movie_1_poster = "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg"
 movie_1_trailer = "https://www.youtube.com/watch?v=vwyZH85NQC4"
-toy_story = media.Movie(movie_1_title, movie_1_story, movie_1_poster, movie_1_trailer)
+toy_story = media.Movie(
+    "Toy Story",
+    "A story of a boy and his toys that come to life",
+    movie_1_poster,
+    movie_1_trailer)
 
-movie_2_title = "Dunkirk"
-movie_2_story = "A movie about the battle of Dunkirk"
 movie_2_poster = "https://i0.wp.com/media2.slashfilm.com/slashfilm/wp/wp-content/images/dunkirk-poster.jpg"
 movie_2_trailer = "https://www.youtube.com/watch?v=F-eMt3SrfFU"
-dunkirk = media.Movie(movie_2_title, movie_2_story, movie_2_poster, movie_2_trailer)
+dunkirk = media.Movie(
+    "Dunkirk",
+    "A movie about the battle of Dunkirk",
+    movie_2_poster,
+    movie_2_trailer)
 
-movie_3_title = "Steve Jobs"
-movie_3_story = "A movie about Steve Jobs"
 movie_3_poster = "https://image.tmdb.org/t/p/original/7j3xCcZZUDTPuOt6KIVDy7xsMyq.jpg"
 movie_3_trailer = "https://www.youtube.com/watch?v=IeOxo7o9T8Q"
-jobs = media.Movie(movie_3_title, movie_3_story, movie_3_poster, movie_3_trailer)
+jobs = media.Movie(
+    "Steve Jobs",
+    "A movie about Steve Jobs",
+    movie_3_poster,
+    movie_3_trailer)
 
-movie_4_title = "Spiderman"
-movie_4_story = "A movie about spiderman"
 movie_4_poster = "http://www.superheromovies.net/img/p/6v0OGYaB8510nH0zcKH0ZWP8HWD.jpg"
 movie_4_trailer = "https://www.youtube.com/watch?v=xEvV3OsE2WM"
-spiderman = media.Movie(movie_4_title, movie_4_story, movie_4_poster, movie_4_trailer)
+spiderman = media.Movie(
+    "Spiderman",
+    "A movie about spiderman",
+    movie_4_poster,
+    movie_4_trailer)
 
-movie_5_title = "Justice League"
-movie_5_story = "No idea but the trailer was cool"
 movie_5_poster = "http://orig04.deviantart.net/7518/f/2016/207/c/2/justice_league_movie____2017__poster_by_mrdeks-dabh40c.jpg"
 movie_5_trailer = "https://www.youtube.com/watch?v=3cxixDgHUYw"
-justice_league = media.Movie(movie_5_title, movie_5_story, movie_5_poster, movie_5_trailer)
+justice_league = media.Movie(
+    "Justice League",
+    "No idea but the trailer was cool",
+    movie_5_poster,
+    movie_5_trailer)
 
-movie_6_title = "Blade runner"
-movie_6_story = "A remake of blade runner"
 movie_6_poster = "https://s-media-cache-ak0.pinimg.com/originals/a8/e9/0a/a8e90a844bed9e6951de16f5f6cde1e0.jpg"
 movie_6_trailer = "https://www.youtube.com/watch?v=gCcx85zbxz4"
-blade_runner = media.Movie(movie_6_title, movie_6_story, movie_6_poster, movie_6_trailer)
+blade_runner = media.Movie(
+    "Blade runner",
+    "A remake of blade runner",
+    movie_6_poster,
+    movie_6_trailer)
 
 # Put the movies in an array and generate the website with fresh_tomatoes
 movies = [toy_story, dunkirk, jobs, spiderman, justice_league, blade_runner]
--- movies/media.py
+++ movies/media.py
@@ -2,14 +2,24 @@ import webbrowser
 
 
 class Movie():
-    """This class stores movie related information"""
+    """This class stores movie related information and can play the trailer in a browser popup"""
 
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
+        """
+        Initializes the movie objet wih the following parameters
+        movie_title: The title of the movie
+        movie_storyline: A short description of what the movie is about
+        poster_image: The URL to the image that will be displayed as the movie poster
+        trailer_youtube: The URL to the trailer on youtube
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """
+        This function displays the movie trailer in a browser popup. 
+        """
         webbrowser.open(self.trailer_youtube_url)
