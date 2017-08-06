9423f6ea05543e0cd964a079f3a4af6757d24e28
charles-ordina <charles.brands@ordina.nl>
2017-08-06 12:23:19 +0200

    fixed long line errors and documentation

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 This project is a website that shows movie trailers.
 It is the first project for the Udacity Full stack developer nanodegree
 
-This program is developed and tested with python version 2.7.10. This code is checked on http://pep8online.com/checkresult.
+This program is developed and tested with python version 2.7.10. This code is checked on [peponline.com](http://pep8online.com/).
 
 ## Requirements
 * Python 2.7.x must be installed.
@@ -12,9 +12,11 @@ This program is developed and tested with python version 2.7.10. This code is ch
 * Unpack the zipfile to a directory of your choice.
 * Start the terminal.
 * Type the following commands
-	* cd (The directory you unpacked the zipfile to.)
-	* cd ufsd-p1-movie-trailer-website
-	* cd movies
-	* python entertainment-center.py
+```
+$ cd (The directory you unpacked the zipfile to.)
+$ cd ufsd-p1-movie-trailer-website
+$ cd movies
+$ python entertainment-center.py
+```
 * The program wil now generate the movies website and open a browswer window showing the posters of 6 movies. 
 * Clicking on a poster will start a trailer of the movie.
--- movies/media.py
+++ movies/media.py
@@ -2,7 +2,10 @@ import webbrowser
 
 
 class Movie():
-    """This class stores movie related information and can play the trailer in a browser popup"""
+    """
+    This class stores movie related information and can play the trailer
+    in a browser popup
+    """
 
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
@@ -10,7 +13,7 @@ class Movie():
         Initializes the movie objet wih the following parameters
         movie_title: The title of the movie
         movie_storyline: A short description of what the movie is about
-        poster_image: The URL to the image that will be displayed as the movie poster
+        poster_image: The URL to the movie poster image
         trailer_youtube: The URL to the trailer on youtube
         """
         self.title = movie_title
@@ -20,6 +23,6 @@ class Movie():
 
     def show_trailer(self):
         """
-        This function displays the movie trailer in a browser popup. 
+        This function displays the movie trailer in a browser popup.
         """
         webbrowser.open(self.trailer_youtube_url)
